357abd726c77a2000b1ddb3e1f7934dbfdfda603
fighterkcs <kchyavansrikar@gmail.com>
2026-01-05 18:17:17 +0530

    UPDATED AGAIN

--- chatbot_pipeline.py
+++ chatbot_pipeline.py
@@ -1,36 +1,31 @@
-
 from huggingface_hub import InferenceClient
 from agents import Agent
 from dotenv import load_dotenv
 import os
 
-
 load_dotenv()
 HF_TOKEN = os.getenv("HUGGINGFACE_API_KEY")
-
-if not HF_TOKEN:
-    raise RuntimeError("HUGGINGFACE_API_KEY not found in .env")
-
-
-WEATHER_API_KEY = "51d28cbcfd3ca09c25084491be6497e2"  
-
+WEATHER_API_KEY = "51d28cbcfd3ca09c25084491be6497e2"
 
 agent = Agent(api_key=WEATHER_API_KEY)
 
+chat_client = InferenceClient(
+    model="HuggingFaceH4/zephyr-7b-beta",
+    token=HF_TOKEN
+)
 
-client = InferenceClient(
-    model="mistralai/Mistral-7B-Instruct-v0.3",
+summary_client = InferenceClient(
+    model="facebook/bart-large-cnn",
     token=HF_TOKEN
 )
 
 def chat_assistant(prompt):
-    
+
     tool_response = agent.perform_task(prompt)
     if tool_response is not None:
         return str(tool_response)
 
-  
-    response = client.chat_completion(
+    response = chat_client.chat_completion(
         messages=[
             {"role": "system", "content": "You are a helpful assistant"},
             {"role": "user", "content": prompt}
@@ -39,4 +34,6 @@ def chat_assistant(prompt):
         temperature=0.7
     )
 
-    return response.choices[0].message["content"]
+    full_answer = response.choices[0].message["content"]
+
+    return full_answer
